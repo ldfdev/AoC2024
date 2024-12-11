12924a19d65446012311eacfe96d3bb065512617
Ldf Develop <ldf.develop@gmail.com>
2024-12-11 11:45:49 +0200

    day 7 part b solution

--- aoc/aoc/day7.py
+++ aoc/aoc/day7.py
@@ -1,10 +1,18 @@
 import dataclasses
+import typing
+import enum
 
 
 @dataclasses.dataclass
 class Equation:
     test_result: int
     operands: list[int]
+
+
+class Operation(enum.Enum):
+    ADDITION = enum.auto()
+    MULTIPLICATION = enum.auto()
+    CONCATENATION = enum.auto()
 
 
 def read_input_file(file_Path: str) -> list[Equation]:
@@ -20,7 +28,36 @@ def read_input_file(file_Path: str) -> list[Equation]:
     return equations
 
 
-def determine_equation_is_valid(test_result: int, operands: list[int]) -> bool:
+def apply_addition(term_1: int, term_2: int) -> typing.Optional[int]:
+    """Checks if ot is possible to apply addition between the 2 terms
+    If so, return the modified term_1 as the result
+    Otehrwise return None"""
+    if term_1 - term_2 >= 0:
+        return term_1 - term_2
+
+
+def apply_multiplication(term_1: int, term_2: int) -> typing.Optional[int]:
+    """Checks if ot is possible to apply multiplication between the 2 terms
+    If so, return the modified term_1 as the result
+    Otehrwise return None"""
+    if term_1 % term_2 == 0:
+        return term_1 // term_2
+
+
+def apply_concatenation(term_1: int, term_2: int) -> typing.Optional[int]:
+    """Checks if ot is possible to apply concatenation between the 2 terms
+    If so, return the modified term_1 as the result
+    Otehrwise return None"""
+    str_term_1, str_term_2 = str(term_1), str(term_2)
+    if str_term_1.endswith(str_term_2):
+         pref: str = str_term_1.removesuffix(str_term_2)
+         if pref:
+             return int(pref)
+
+
+def determine_equation_is_valid(
+    test_result: int, operands: list[int], allowed_operations: list[Operation]
+) -> bool:
     """determines if appying addition and multiplication to opeeands
     evaluating them left to right can obtain test_result"""
     if not operands:
@@ -28,21 +65,54 @@ def determine_equation_is_valid(test_result: int, operands: list[int]) -> bool:
     if len(operands) == 1:
         return test_result == operands[0]
     first, last = operands[:-1], operands[-1]
-    multiplication_result, addition_result = False, False
-    if test_result % last == 0:
-        # we can possibly apply multiplication as the last operation
-        multiplication_result: bool = determine_equation_is_valid(
-            test_result // last, first
-        )
-    if test_result - last >= 0:
-        # we can possibly apply addition as the last operation
-        addition_result: bool = determine_equation_is_valid(test_result - last, first)
-    return addition_result or multiplication_result
+
+    operation_results: list[bool] = list()
+
+    if Operation.MULTIPLICATION in allowed_operations:
+        multiplication_result = apply_multiplication(test_result, last)
+        if multiplication_result is not None:
+            multiplication_solution: bool = determine_equation_is_valid(
+                multiplication_result, first, allowed_operations
+            )
+            operation_results.append(multiplication_solution)
+
+    if Operation.ADDITION in allowed_operations:
+        addition_result = apply_addition(test_result, last)
+        if addition_result is not None:
+            addition_solution: bool = determine_equation_is_valid(
+                addition_result, first, allowed_operations
+            )
+            operation_results.append(addition_solution)
+
+    if Operation.CONCATENATION in allowed_operations:
+        concatenation_result = apply_concatenation(test_result, last)
+        if concatenation_result is not None:
+            concatenationsolution: bool = determine_equation_is_valid(
+                concatenation_result, first, allowed_operations
+            )
+            operation_results.append(concatenationsolution)
+
+    return any(operation_results)
 
 
 def solve_part_a(equations: list[Equation]) -> int:
     return sum(
         equation.test_result
         for equation in equations
-        if determine_equation_is_valid(equation.test_result, equation.operands)
+        if determine_equation_is_valid(
+            equation.test_result,
+            equation.operands,
+            [Operation.ADDITION, Operation.MULTIPLICATION],
+        )
+    )
+
+def solve_part_b(equations: list[Equation]) -> int:
+    return sum(
+        equation.test_result
+        for equation in equations
+        if determine_equation_is_valid(
+            equation.test_result,
+            equation.operands,
+            [Operation.ADDITION, Operation.MULTIPLICATION, Operation.CONCATENATION],
+        )
     )
--- tests/test_day_7.py
+++ tests/test_day_7.py
@@ -6,12 +6,24 @@ input_data: list[day7.Equation] = day7.read_input_file(day_5_file.as_posix())
 
 
 def test_determine_equation_is_valid():
-    assert day7.determine_equation_is_valid(100, [10, 10]) is True
-    assert day7.determine_equation_is_valid(110, [10, 10, 10]) is True
-    assert day7.determine_equation_is_valid(10, [10]) is True
+    allowed_operations: list[day7.Operation] = [
+        day7.Operation.ADDITION,
+        day7.Operation.MULTIPLICATION,
+    ]
+    assert day7.determine_equation_is_valid(100, [10, 10], allowed_operations) is True
+    assert (
+        day7.determine_equation_is_valid(110, [10, 10, 10], allowed_operations) is True
+    )
+    assert day7.determine_equation_is_valid(10, [10], allowed_operations) is True
 
-    assert day7.determine_equation_is_valid(100, [1, 10]) is False
-    assert day7.determine_equation_is_valid(54, [10]) is False
+    assert day7.determine_equation_is_valid(100, [1, 10], allowed_operations) is False
+    assert day7.determine_equation_is_valid(54, [10], allowed_operations) is False
+
+    assert day7.determine_equation_is_valid(1212, [12, 12], allowed_operations) is False
+    assert day7.determine_equation_is_valid(1212, [12, 12], [op for op in day7.Operation]) is True
 
 def test_solve_part_a():
     assert day7.solve_part_a(input_data) == 3749
+
+def test_solve_part_b():
+    assert day7.solve_part_b(input_data) == 11387
