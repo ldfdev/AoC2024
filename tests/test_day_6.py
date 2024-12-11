73737dad2c939c2e071169b740bbe284e2f13b64
Ldf Develop <ldf.develop@gmail.com>
2024-12-11 10:10:49 +0200

    impleent a bad solution where a new obstacle is placed near the first cell visited for a second time

--- tests/test_day_6.py
+++ tests/test_day_6.py
@@ -7,3 +7,7 @@ input_data: day6.Puzzle = day6.read_input_file(day_6_file.as_posix())
 
 def test_solve_part_a():
     assert day6.solve_part_a(input_data) == 41
+
+
+def test_solve_part_b():
+    assert day6.solve_part_b(input_data) == 6
